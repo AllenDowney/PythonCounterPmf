24a5d28760bde985ea99b9c0726c601df79adc6c
Allen Downey <downey@allendowney.com>
2014-05-06 16:27:20 -0400

    Adding ipython notebook

--- pmf_example.py
+++ pmf_example.py
@@ -4,45 +4,11 @@ Copyright 2013 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
-from collections import Counter
-import myplot
+import suite
+import matplotlib.pyplot as pyplot
 
-
-class Pmf(Counter):
-    """A Counter with probabilities."""
-
-    def Normalize(self):
-        """Normalizes the PMF so the probabilities add to 1."""
-        total = float(sum(self.values()))
-        for key in self:
-            self[key] /= total
-
-    def __add__(self, other):
-        """Adds two distributions.
-
-        The result is the distribution of sums of values from the
-        two distributions.
-
-        other: Pmf
-
-        returns: new Pmf
-        """
-        pmf = Pmf()
-        for key1, prob1 in self.items():
-            for key2, prob2 in other.items():
-                pmf[key1 + key2] += prob1 * prob2
-        return pmf
-
-    def __hash__(self):
-        return id(self)
-
-    def Render(self):
-        """Returns values and their probabilities, suitable for plotting."""
-        return zip(*sorted(self.items()))
-
-
-d6 = Pmf([1,2,3,4,5,6])
-d6.Normalize()
+d6 = suite.Pmf([1,2,3,4,5,6])
+d6.normalize()
 print d6
 d6.name = 'one die'
 
@@ -52,18 +18,19 @@ d6_twice.name = 'two dice'
 for key, prob in d6_twice.items():
     print key, prob
 
-pmf_ident = Pmf([0])
+pmf_ident = suite.Pmf([0])
 d6_thrice = sum([d6]*3, pmf_ident)
 d6_thrice.name = 'three dice'
 
 for key, prob in d6_thrice.items():
     print key, prob
 
-myplot.PrePlot(3)
-myplot.Pmf(d6)
-myplot.Pmf(d6_twice)
-myplot.Pmf(d6_thrice)
-myplot.Save(root='pmf_example',
-            xlabel='Total',
-            ylabel='Probability')
+for die in [d6, d6_twice, d6_thrice]:
+    xs, ys = die.render()
+    pyplot.plot(xs, ys, label=die.name, linewidth=2, alpha=0.5)
+    
+pyplot.xlabel('Total')
+pyplot.ylabel('Probability')
+pyplot.legend()
+pyplot.show()
 
--- suite_example.py
+++ suite_example.py
@@ -4,73 +4,32 @@ Copyright 2013 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
-from collections import Counter
+import suite
 
-
-class Pmf(Counter):
-    """A Counter with probabilities."""
-
-    def normalize(self):
-        """Normalizes the PMF so the probabilities add to 1."""
-        total = float(sum(self.values()))
-        for key in self:
-            self[key] /= total
-
-    def __add__(self, other):
-        """Adds two distributions.
-
-        The result is the distribution of sums of values from the
-        two distributions.
-
-        other: Pmf
-
-        returns: new Pmf
-        """
-        pmf = Pmf()
-        for key1, prob1 in self.items():
-            for key2, prob2 in other.items():
-                pmf[key1 + key2] += prob1 * prob2
-        return pmf
-
-    def __hash__(self):
-        return id(self)
-
-    def render(self):
-        """Returns values and their probabilities, suitable for plotting."""
-        return zip(*sorted(self.items()))
-
-
-class Suite(Pmf):
-    """Map from hypothesis to probability."""
-
-    def update(self, data):
-        """
-        """
-        for hypo in self:
-            like = self.Likelihood(data, hypo)
-            self[hypo] *= like
-
-        self.normalize()
-
-
-class DiceSuite(Suite):
+class DiceSuite(suite.Suite):
     
     def likelihood(self, data, hypo):
+        """Computes the likelihood of the data under the hypothesis.
+
+        data: result of a die roll
+        hypo: Die object
+        """
         return hypo[data]
         
 
 def make_die(num_sides):
-    die = Pmf(range(1, num_sides+1))
+    die = suite.Pmf(range(1, num_sides+1))
     die.name = 'd%d' % num_sides
     die.normalize()
     return die
 
 
 dice = [make_die(x) for x in [4, 6, 8, 12, 20]]
+dice_suite = DiceSuite(dice)
 
-metapmf = DiceSuite(dice)
-metapmf.update(6)
+dice_suite.bayesian_update(6)
 
-for die, prob in sorted(metapmf.items()):
+
+for die, prob in sorted(dice_suite.items()):
     print die.name, prob
 
